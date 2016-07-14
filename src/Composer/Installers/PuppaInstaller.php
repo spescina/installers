c5f7935faa818c98b2c6703f4268f94003dbe7b8
Simone Pescina <s.pescina@gmail.com>
2016-07-15 00:16:17 +0200

    Create PuppaInstaller.php

---
+++ src/Composer/Installers/PuppaInstaller.php
@@ -0,0 +1,37 @@
+<?php
+namespace Composer\Installers;
+class PuppaInstaller extends BaseInstaller
+{
+    protected $locations = array(
+        'module' => 'Modules/{$name}/',
+        'theme' => 'Themes/{$name}/'
+    );
+    /**
+     * Format package name.
+     *
+     * For package type asgard-module, cut off a trailing '-plugin' if present.
+     *
+     * For package type asgard-theme, cut off a trailing '-theme' if present.
+     *
+     */
+    public function inflectPackageVars($vars)
+    {
+        if ($vars['type'] === 'puppa-module') {
+            return $this->inflectPluginVars($vars);
+        }
+        if ($vars['type'] === 'puppa-theme') {
+            return $this->inflectThemeVars($vars);
+        }
+        return $vars;
+    }
+    protected function inflectPluginVars($vars)
+    {
+        $vars['name'] = ucfirst(preg_replace('/-module/', '', $vars['name']));
+        return $vars;
+    }
+    protected function inflectThemeVars($vars)
+    {
+        $vars['name'] = ucfirst(preg_replace('/-theme$/', '', $vars['name']));
+        return $vars;
+    }
+}
